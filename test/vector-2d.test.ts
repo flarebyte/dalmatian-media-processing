45ec4b445dbbdcf1c4a038862ad7af4b4ec8d344
Olivier Huin <olih@users.noreply.github.com>
2022-05-28 11:19:34 +0100

    Updated snaphot

--- test/vector-2d.test.ts
+++ test/vector-2d.test.ts
@@ -1,8 +1,23 @@
 import { V2d } from '../src/vector-2d';
 
 describe('vector-2d', () => {
-  it('should provide', () => {
-    const actual = V2d.fromString('1/2 1/4')
-    expect(actual).toBeDefined()
+  it('should display the right representation', () => {
+    const values = ['1/2 1/4', '-1/2 -1/3'];
+    const actual = values.map(V2d.fromString).map((v) => ({
+      dalmatian: v.toDalmatianString(),
+      cartesian: v.toCartesianString(300),
+    }));
+    expect(actual).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cartesian": "(150.000,75.000)",
+          "dalmatian": "0.5 0.25",
+        },
+        Object {
+          "cartesian": "(-150.000,-100.000)",
+          "dalmatian": "-0.5 -0.(3)",
+        },
+      ]
+    `);
   });
 });
