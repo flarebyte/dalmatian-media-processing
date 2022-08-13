975502c7362febe54d06c8a9c01c647be92cdc86
Olivier Huin <olih@users.noreply.github.com>
2022-08-13 17:20:50 +0100

    split string to object

--- src/split-utils.ts
+++ src/split-utils.ts
@@ -21,6 +21,21 @@ function splitStringNotAssert<L extends number>(
   return fields.map((_field, index) => stringArray.at(index) || '');
 }
 
+function splitStringAsObject<L extends number>(
+  line: string,
+  separator: string,
+  fields: FixedLengthStringArray<L>
+): { [k: string]: string} {
+  const stringArray = line.split(separator).filter(isString);
+  for (const [index, field] of fields.entries()) {
+    if (typeof stringArray.at(index) === undefined) {
+      throw new Error(`Field ${field} is expected in ${line}`);
+    }
+  }
+  const keyValues = fields.map((field, index) => [field, stringArray.at(index) || '']);
+  return Object.fromEntries(keyValues);
+}
+
 export function split2Strings(
   line: string,
   separator: string,
@@ -28,4 +43,13 @@ export function split2Strings(
 ): FixedLengthStringArray<2> {
   const items = splitStringNotAssert<2>(line, separator, fields);
   return [items.at(0) || '', items.at(1) || ''];
+}
+
+export function split2StringsAsObject(
+  line: string,
+  separator: string,
+  fields: FixedLengthStringArray<2>
+):  { [k: string]: string} {
+  const obj = splitStringAsObject<2>(line, separator, fields);
+  return obj;
 }
